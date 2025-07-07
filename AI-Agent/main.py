db273f61ae57ef85924630f2085f2db8d9cc0ca9
Mustafa Ahsan <optaycal@gmail.com>
2025-07-07 16:38:59 +0500

    Added System Message prompt

--- AI-Agent/main.py
+++ AI-Agent/main.py
@@ -1,26 +1,27 @@
 from langchain_core.messages import HumanMessage
+from langchain_core.messages import SystemMessage
 from langchain_openai import ChatOpenAI
 from langchain.tools import tool
 from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
 from openai import OpenAI
+import numpy as np
+from langchain_openai import OpenAIEmbeddings
 import os
 
 load_dotenv()
 
-@tool
-
 
 def main():
-    model = ChatOpenAI(
-    temperature=0,
-    base_url="https://openrouter.ai/api/v1",  # ✅ Force use of OpenRouter
-    api_key=os.getenv("OPENAI_API_KEY"),       # ✅ Load from your .env
-    model="deepseek/deepseek-r1-0528:free"  # ✅ Use correct model name
-)
 
+    model = ChatOpenAI(
+        temperature=0,
+        base_url="https://openrouter.ai/api/v1",
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model="deepseek/deepseek-r1-0528:free")
+    prompt = "You are an educational assistant focused strictly on education-related topics, especially study abroad programs, universities, scholarships, academic life, exams, etc. If the user asks anything unrelated to education or study abroad, politely respond that you can only help with educational questions."
     tools = []
-    agent_executor = create_react_agent(model, tools)
+    agent_executor = create_react_agent(model, tools, prompt=prompt)
 
     print("Welcome to the AI Agent!\nType 'exit' to quit the program.")
 
